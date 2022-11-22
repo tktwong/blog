44d15b3d0c183419a9b2558f80b7fb05bf902ec6
TERRY WONG <iamtktwong@gmail.com>
2022-11-22 02:25:51 +0000

    chore: update

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 import blog from "https://deno.land/x/blog@0.5.0/blog.tsx";
 
 blog({
-  title: "Hardys Blog",
-  author: "Hardys",
+  title: "Terry's Blog",
+  author: "Hardys' mentee",
   avatar: "./avatar.jpg",
   avatarClass: "rounded-full",
   links: [
-    { title: "GitHub", url: "https://github.com/hardyscc" },
-    { title: "Twitter", url: "https://twitter.com/hardyscchk" },
+    { title: "GitHub", url: "https://github.com/tktwong" },
+    { title: "Twitter", url: "https://twitter.com/iamtktwong" },
   ],
 });
